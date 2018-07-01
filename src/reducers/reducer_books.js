27d8b12186969654975d96f0418a0143d88bfd6f
san <metternichch@gmail.com>
2018-07-01 17:50:57 +0900

    BookList
    BookList

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+  return [
+    {title: 'javascript: The Good Parts', pages: 101 },
+    {title: 'javascript: Harry Potter', pages: 39 },
+    {title: 'javascript: The Dark Tower', pages: 85 },
+    {title: 'javascript: Eloquent Ruby', pages: 1 }
+  ]
+}
